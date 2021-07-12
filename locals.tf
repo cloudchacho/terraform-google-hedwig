e0b3f1b960d455aa0425db1a07813b15b16ad495
Aniruddha Maru <maru@standard.ai>
2021-07-12 13:10:16 -0700

    use subscription's key

--- locals.tf
+++ locals.tf
@@ -3,16 +3,19 @@ locals {
     for merged_subscription in flatten([
       for name, consumer in var.pull_consumers : [
         for topic, subscription in consumer.subscriptions : {
-          queue                        = name
-          labels                       = consumer.labels
-          service_account              = consumer.service_account
-          topic                        = subscription.topic != null ? subscription.topic : topic
-          project                      = subscription.project
-          enable_ordering              = subscription.enable_ordering
-          high_message_count_threshold = subscription.high_message_count_threshold
+          key = "${name}-${topic}"
+          value = {
+            queue                        = name
+            labels                       = consumer.labels
+            service_account              = consumer.service_account
+            topic                        = subscription.topic != null ? subscription.topic : topic
+            project                      = subscription.project
+            enable_ordering              = subscription.enable_ordering
+            high_message_count_threshold = subscription.high_message_count_threshold
+          }
         }
       ]
     ]) :
-    (merged_subscription.project != null ? "${merged_subscription.queue}-${merged_subscription.project}-${merged_subscription.topic}" : "${merged_subscription.queue}-${merged_subscription.topic}") => merged_subscription
+    merged_subscription.key => merged_subscription.value
   }
 }
