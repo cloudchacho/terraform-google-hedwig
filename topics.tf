8afaca351478f32793f1ce85f763495e8eeaa40c
Max Bigras <max@standard.ai>
2023-12-05 23:15:05 -0800

    Add firehose reloaded; remove dataflow

--- topics.tf
+++ topics.tf
@@ -1,23 +1,17 @@
 module "topics" {
   for_each = var.topics
 
-  source  = "cloudchacho/hedwig-topic/google"
-  version = ">= 2.2, <3"
+  # source  = "cloudchacho/hedwig-topic/google"
+  # version = ">= 2.2, <3"
+  # source = "../terraform-google-hedwig-topic"
+  source = "git::https://github.com/max-standard/terraform-google-hedwig-topic.git?ref=firehose-reloaded"
 
   topic = each.key
 
   enable_firehose_all_messages = var.enable_firehose_all_topics || each.value.enable_firehose == true
+  firehose_bucket              = each.value.firehose_bucket
+  firehose_prefix              = each.value.firehose_prefix
 
   iam_service_accounts = each.value.service_accounts
   iam_members          = each.value.iam_members
-
-  enable_alerts    = var.enable_alerts
-  alerting_project = var.alerting_project
-
-  dataflow_freshness_alert_notification_channels = var.dataflow_alert_notification_channels
-  dataflow_tmp_gcs_location                      = var.dataflow_tmp_gcs_location
-  dataflow_template_gcs_path                     = var.dataflow_template_pubsub_to_storage_gcs_path
-  dataflow_zone                                  = var.dataflow_zone
-  dataflow_region                                = var.dataflow_region
-  dataflow_output_directory                      = var.dataflow_output_directory
 }
--- variables.tf
+++ variables.tf
@@ -1,28 +1,3 @@
-variable "dataflow_tmp_gcs_location" {
-  default     = ""
-  description = "A gs bucket location for storing temporary files by Google Dataflow, e.g. gs://myBucket/tmp"
-}
-
-variable "dataflow_template_pubsub_to_storage_gcs_path" {
-  default     = "gs://dataflow-templates/2019-04-03-00/Cloud_PubSub_to_GCS_Text"
-  description = "The template path for Google Dataflow, e.g. gs://dataflow-templates/2019-04-24-00/Cloud_PubSub_to_GCS_Text"
-}
-
-variable "dataflow_zone" {
-  default     = ""
-  description = "The zone to use for Dataflow. This may be required if it's not set at the provider level, or that zone doesn't support Dataflow regional endpoints (see https://cloud.google.com/dataflow/docs/concepts/regional-endpoints)"
-}
-
-variable "dataflow_region" {
-  default     = ""
-  description = "The region to use for Dataflow. This may be required if it's not set at the provider level, or you want to use a region different from the zone (see https://cloud.google.com/dataflow/docs/concepts/regional-endpoints)"
-}
-
-variable "dataflow_output_directory" {
-  default     = ""
-  description = "A gs bucket location for storing output files by Google Dataflow, e.g. gs://myBucket/hedwigBackup"
-}
-
 variable "enable_firehose_all_topics" {
   default     = false
   description = "Enable firehose for all messages on all topics"
@@ -36,12 +11,6 @@ variable "enable_alerts" {
 variable "alerting_project" {
   default     = ""
   description = "The project id to create monitoring alert policies"
-}
-
-variable "dataflow_alert_notification_channels" {
-  default     = []
-  type        = list(string)
-  description = "List of Stackdriver notification channels for Firehose dataflow data freshness stale alert"
 }
 
 variable "dlq_alert_notification_channels" {
@@ -113,8 +82,14 @@ variable "topics" {
   description = "List of Hedwig topics"
   default     = {}
   type = map(object({
-    # Firehose all messages published to this topic into GCS
+    # Firehose—that is, save—all messages published to this topic into GCS
     enable_firehose = optional(bool)
+
+    # Variable firehose_bucket declares the bucket for firehose—that is, saved—messages (should already exist)
+    firehose_bucket = optional(string)
+
+    # Variable firehose_prefix declares the prefix for firehose—that is, saved—messages. Note: The "<topic>" string is replaced by var.topic; for example, "myenv/<topic>/" variable becomes "myenv/mytopic/" string. This confusing approach enables prefixing all topics in a for-loop.
+    firehose_prefix = optional(string)
 
     # DEPRECATED: use `iam_members` instead
     # service accounts for publishing permissions
