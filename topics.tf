84cf150c6c7daed4dbd34780c4b887cc80d813c7
Max Bigras <max@standard.ai>
2024-01-08 16:53:32 -0800

    Typo: Reset hedwig-topic

--- topics.tf
+++ topics.tf
@@ -1,10 +1,8 @@
 module "topics" {
   for_each = var.topics
 
-  # source  = "cloudchacho/hedwig-topic/google"
-  # version = ">= 2.2, <3"
-  source = "/Users/max/terraform-google-hedwig-topic"
-  # source = "git::https://github.com/max-standard/terraform-google-hedwig-topic.git?ref=firehose-reloaded"
+  source  = "cloudchacho/hedwig-topic/google"
+  version = ">= 2.2, <3"
 
   topic                = each.key
   firehose_config      = each.value.firehose_config
